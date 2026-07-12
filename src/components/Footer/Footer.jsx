8b41f67b0521cf8808740b4911f45e29822b964b
JOHNATHAN VIRGIL <JOHNATHANVIRGIL@YMAIL.COM>
2026-07-11 23:13:41 -0700

    final fixes before submission

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,51 @@
+import { Link } from "react-router-dom";
+import githubIcon from "../../assets/Github.svg";
+import linkedinIcon from "../../assets/LinkedIn.svg";
 import "./Footer.css";
 
 function Footer() {
   return (
     <footer className="footer">
-      <p className="footer__copyright">
-        © 2026 Supersite, Powered by News API
-      </p>
+      <div className="footer__content">
+        <div className="footer__top">
+          <Link to="/" className="footer__link">
+            Home
+          </Link>
+
+          <div className="footer__social">
+            <a
+              href="https://github.com/yourusername"
+              target="_blank"
+              rel="noreferrer"
+              className="footer__icon-link"
+            >
+              <img src={githubIcon} alt="GitHub" className="footer__icon" />
+            </a>
+
+            <a
+              href="https://www.linkedin.com/in/yourusername/"
+              target="_blank"
+              rel="noreferrer"
+              className="footer__icon-link"
+            >
+              <img src={linkedinIcon} alt="LinkedIn" className="footer__icon" />
+            </a>
+          </div>
+        </div>
+
+        <a
+          href="https://tripleten.com"
+          target="_blank"
+          rel="noreferrer"
+          className="footer__link"
+        >
+          TripleTen
+        </a>
+
+        <p className="footer__copyright">
+          © 2026 Supersite, Powered by News API
+        </p>
+      </div>
     </footer>
   );
 }
